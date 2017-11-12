f6d525522a04373a31f00e3213a5a4e77db79920
peikai zheng <bnuzpk@gmail.com>
2017-11-13 04:46:55 +0800

    add wifi_map

---
+++ script/wifi_map.py
@@ -0,0 +1,72 @@
+# coding: utf-8
+
+import csv
+import os
+import pickle
+
+class WifiMap(object):
+    def __init__(self, wifi_dir):
+        self.pickle_file = os.path.join(wifi_dir, 'wifi_map.pickle')
+        if os.path.isfile(self.pickle_file):
+
+            f = open(self.pickle_file, 'rb')
+            self.wifi_map = pickle.load(f)
+        else:
+            self.wifi_map = self._init(wifi_dir);
+
+            with open(self.pickle_file, 'wb') as fout:
+                pickle.dump(self.wifi_map, fout)
+                fout.close()
+
+        self.wifi_num = len(self.wifi_map)
+
+
+    def _init(self, wifi_dir):
+        '''
+        load the 'wifi' column of
+        user_shop_behavior.csv & evaluation_public.csv
+        to wifi_map
+        :param wifi_dir: directory of wifi data
+        :return: map<wifi_name, int_id>
+        '''
+        user_shop_file = os.path.join(
+            wifi_dir,
+            'ccf_first_round_user_shop_behavior.csv'
+        )
+        wifi_map = {}
+        wifi_num = 0
+        with open(user_shop_file) as fin:
+            reader = csv.DictReader(fin)
+            for line in reader:
+                for wifi_items in line['wifi_infos'].split(';'):
+                    item = wifi_items.split('|')
+                    assert len(item) == 3;
+                    wifi_id = item[0]
+                    if wifi_id not in wifi_map:
+                        wifi_map[wifi_id] = wifi_num
+                        wifi_num += 1
+
+        evaluation_file = os.path.join(
+            wifi_dir,
+            'evaluation_public.csv'
+        )
+        with open(evaluation_file) as fin:
+            reader = csv.DictReader(fin)
+            for line in reader:
+                for wifi_items in line['wifi_infos'].split(';'):
+                    item = wifi_items.split('|')
+                    assert len(item) == 3
+                    wifi_id = item[0]
+                    if wifi_id not in wifi_map:
+                        wifi_map[wifi_id] = wifi_num
+                        wifi_num += 1
+        return wifi_map
+
+    def GetIndex(self, wifi_id):
+        '''
+        hash wifi_id to int index
+        :param wifi_id: type str; like 'b_6396480'
+        :return: int
+        '''
+        assert wifi_id in self.wifi_map
+        return self.wifi_map[wifi_id]
