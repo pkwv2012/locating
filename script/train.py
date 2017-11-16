e979fe6f07838975cff5f5d541ec8d5638f3dd04
peikai zheng <bnuzpk@gmail.com>
2017-11-15 20:51:18 -0800

    tune param

--- script/train.py
+++ script/train.py
@@ -61,7 +61,9 @@ def ProcessFeatures(filepath, wifi_hashmap, mall_shop_hashmap, lng_lat, max_dist
             lng, lat = float(line['longitude']), float(line['latitude'])
             # filter (lng, lat) abnormal data
             if 'shop_id' in line:
+                shop_id = line['shop_id']
                 dist = math.sqrt((lng - lng_lat[shop_id][0])**2 + (lat - lng_lat[shop_id][1])**2)
+                assert shop_id in max_dist
                 if dist > max_dist[shop_id]:
                     continue
             indptr[mall_id].append(len(indices[mall_id]))
@@ -132,6 +134,7 @@ def GetShopMaxDist(shop_info_filepath, lng_lat):
             )
         max_dist = defaultdict(float)
         for shop_id in shop_user_dist_list:
+            LOGGER.info('shop_size={}'.format(len(shop_user_dist_list)))
             shop_user_dist_list[shop_id].sort()
             l = len(shop_user_dist_list[shop_id])
             max_dist[shop_id] = shop_user_dist_list[shop_id][int(l * 0.9)] if l > 10 else \
@@ -142,11 +145,13 @@ def GetShopMaxDist(shop_info_filepath, lng_lat):
 def Train(data_dir, wifi_hashmap, mall_shop_hashmap):
     shop_info_file = os.path.join(data_dir, Config.shop_info_filename)
     lng_lat = GetShopLngLat(shop_info_file)
-    max_dist = GetShopMaxDist(shop_info_file, lng_lat)
     train_file = os.path.join(data_dir, Config.user_shop_filename)
+    max_dist = GetShopMaxDist(train_file, lng_lat)
     dtrain_dict, row_id = GetFeatures(train_file, wifi_hashmap, mall_shop_hashmap, lng_lat, max_dist)
+    LOGGER.info('load train data finish')
     test_file = os.path.join(data_dir, Config.evaluation_filename)
     dtest_dict, row_id = GetFeatures(test_file, wifi_hashmap, mall_shop_hashmap, lng_lat, max_dist)
+    LOGGER.info('load test data finish')
     assert dtrain_dict.keys() == dtest_dict.keys()
 
     # setup parameters for xgboost
@@ -155,9 +160,11 @@ def Train(data_dir, wifi_hashmap, mall_shop_hashmap):
     param['objective'] = 'multi:softmax'
     # scale weight of positive examples
     param['eta'] = 0.1
-    param['max_depth'] = 4
+    param['max_depth'] = 3
     param['silent'] = 1
-    param['min_child_weight'] = 4
+    param['min_child_weight'] = 3
+    param['subsample'] = 0.8
+    param['lambda'] = 0.5
     # param['nthread'] = 2
     result = defaultdict(list)
     time_suffix = datetime.now().strftime('%Y_%m_%d_%H_%M')
@@ -169,8 +176,8 @@ def Train(data_dir, wifi_hashmap, mall_shop_hashmap):
         early_stop_round = 10
         if Config.is_train:
             error_list = xgb.cv(param, dtrain_dict[key],
-                         num_boost_round=100,
-                         nfold=4,
+                         num_boost_round=300,
+                         nfold=3,
                          early_stopping_rounds=early_stop_round
                          )
             booster = xgb.train(param, dtrain_dict[key],
